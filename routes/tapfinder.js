ed1a22f6557d83cb5e9991ad443ed5c48fc47ba4
Charles Treatman <charles.treatman@gmail.com>
2015-07-20 15:25:45 -0400

    Searches for beers on PhillyTapFinder

--- routes/tapfinder.js
+++ routes/tapfinder.js
@@ -1,9 +1,55 @@
+var _ = require('underscore');
+var async = require('async');
+var cheerio = require('cheerio');
 var express = require('express');
+var superagent = require('superagent');
 var router = express.Router();
+
+var tapfinderBaseUrl = 'http://www.phillytapfinder.com';
 
 /* GET beer search. */
 router.get('/beer', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  if (req.query.search) {
+    searchTapfinder(req.query.search, function(response) {
+      loadBeers(response, function(results) {
+        res.send({ beers: results });
+      });
+    });
+  }
+  else {
+    res.status(400)
+    res.send({ error: 'You must provide a search term in the "search" query parameter' });
+  }
 });
 
 module.exports = router;
+
+function searchTapfinder(searchTerm, callback) {
+  superagent.post(tapfinderBaseUrl + '/wp-content/plugins/phillytapfinder/')
+    .type('form').send({ "class": 'Search', "process": 'searchAll', "searchTerm": searchTerm })
+    .end(function(error, response) {
+      callback(response.body);
+    });
+}
+
+function loadBeers(searchResults, callback) {
+  var tasks = _.map(searchResults.beers, loadBeer);
+  async.parallel(tasks, function(err, results) {
+    callback(results);
+  });
+}
+
+function loadBeer(beer) {
+  return function(callback) {
+    superagent.get(tapfinderBaseUrl + beer.link).end(function(error, response) {
+      var $ = cheerio.load(response.text),
+          beerAsJson = {
+            name: $('#tap-detail .tap-list .tap-list-name').text(),
+            origin: $('#tap-detail .tap-list .origin > strong').text(),
+            style: $('#tap-detail .tap-list .origin strong:first-child').text()
+          };
+
+      callback(null, beerAsJson);
+    });
+  }
+}
--- test/app_test.js
+++ test/app_test.js
@@ -4,7 +4,27 @@ var request = require('supertest'),
 var app = require('../app.js');
 
 describe('GET /beer', function() {
-  it('responds with a success status', function(done) {
-    request(app).get('/beer').expect(200, done);
+  this.timeout(4000);
+
+  describe('when search text is provided', function() {
+    it('responds with a success status', function(done) {
+      request(app).get('/beer?search=consecration').expect(200, done);
+    });
+
+    it('returns the tapfinder results in the "beers" element', function(done) {
+      request(app).get('/beer?search=consecration').expect({
+        beers: [{
+          name: 'Russian River Consecration',
+          origin: 'Santa Rosa, CA',
+          style: 'Sour/Wild-Fermented Ale'
+        }]
+      }, done);
+    });
+  });
+
+  describe('when no search text is provided', function() {
+    it('responds with a bad request status', function(done) {
+      request(app).get('/beer').expect(400, done);
+    });
   });
 });
